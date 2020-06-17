8eca76bd5c1ea1cce92843eceafcd40db9d71e09
fanuelnalem <fanuelnalem@outlook.com>
2020-06-16 19:34:48 -0700

    Ref

--- index.js
+++ index.js
@@ -4,25 +4,13 @@ const mongoose = require('mongoose');
 
 
 const app = express();
-// const server = require('http').createServer(app);
-// const io = require('socket.io')(server);
+ 
 
 const routes = require('./routes');
 
 const PORT = process.env.PORT || 3001;
 
-
-// io.on('connection', socket => {
-
-//   socket.on('message', (data) => {
-//     socket.emit('serverToClientMessage', data);
-//   });
-
-//   socket.on('disconnect', () => {
-//     console.log("See ya");
-//     return;
-//   });
-// });
+ const routet = require('./client/public/index.html')
 
 // Setup middlewares
 app.use(express.urlencoded({ extended: true }));
@@ -32,7 +20,15 @@ app.use(express.json());
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
 }
- 
+
+app.get('*', function(req, res) {
+  res.sendFile(path.join(__dirname, './client/public/index.html'), function(err) {
+    if (err) {
+      res.status(500).send(err)
+    }
+  })
+})
+
 app.use("/",routes);
 require('./services/passport');
 // Connect database
